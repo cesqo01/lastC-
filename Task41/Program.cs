af78fc5346770520ccd88c3bae8c0ce16c9d4970
Dmitriy <dyakonov.dmitriy01@gmail.com>
2023-06-07 02:50:19 +0300

    Tasks41,43

---
+++ Task41/Program.cs
@@ -0,0 +1,50 @@
+﻿/*
+Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+-1, -7, 567, 89, 223-> 3
+*/
+
+int[] CreateArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = EnterNumber($"Введите {i + 1}й элемент");
+    }
+    return array;
+}
+
+void PrintArray(int[] arr)
+
+{
+    
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write($"{arr[i]}, ");
+    }
+    Console.Write($"{arr[arr.Length - 1]}");
+}
+
+int CountPositive(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0) count += 1;
+    }
+    return count;
+}
+
+int EnterNumber(string message)
+
+{
+    Console.WriteLine(message);
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+int count = EnterNumber("Введите количество чисел");
+int[] array = CreateArray(count);
+PrintArray(array);
+int countPositive = CountPositive(array);
+Console.WriteLine($" -> {countPositive}");
---
+++ Task43/Program.cs
@@ -0,0 +1,55 @@
+﻿/*
+Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+*/
+
+double[] DotCrossing(double b1, double k1, double b2, double k2)
+{
+    double x = Math.Round((b2 - b1) / (k1 - k2), 1);
+    double y = Math.Round(k1 * x + b1, 1);
+    double[] array = { x, y };
+    return array;
+}
+
+string CrossingExist(double b1, double k1, double b2, double k2)
+{
+    if (k1 == k2)
+    {
+        if (b1 == b2)
+        {
+            return ("The lines are identical");
+
+        }
+
+        return ("The lines are parallel");
+    }
+    return $"Точка пересечения прямых -> ";
+}
+
+double EnterNumber(string message)
+
+{
+    Console.WriteLine(message);
+    double num = Math.Round(Convert.ToDouble(Console.ReadLine()), 1);
+    return num;
+}
+
+void PrintArray(double[] arr)
+{
+
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write($"({arr[i]}; ");
+    }
+    Console.Write($"{arr[arr.Length - 1]})");
+}
+
+double b1 = EnterNumber("Введите коэффициент первой прямой");
+double k1 = EnterNumber("Введите угловой коэффициент первой прямой");
+double b2 = EnterNumber("Введите коэффициент второй прямой");
+double k2 = EnterNumber("Введите угловой коэффициент второй прямой");
+Console.Write(CrossingExist(b1, k1, b2, k2));
+double[] array = DotCrossing(b1, k1, b2, k2);
+PrintArray(array);
+
