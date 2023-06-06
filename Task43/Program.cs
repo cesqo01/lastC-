a92138960839abbc6d69199cc599addad7d80142
Dmitriy <dyakonov.dmitriy01@gmail.com>
2023-06-07 02:55:14 +0300

    m

--- Task43/Program.cs
+++ Task43/Program.cs
@@ -12,20 +12,20 @@ double[] DotCrossing(double b1, double k1, double b2, double k2)
     return array;
 }
 
-string CrossingExist(double b1, double k1, double b2, double k2)
-{
-    if (k1 == k2)
-    {
-        if (b1 == b2)
-        {
-            return ("The lines are identical");
+// string CrossingExist(double b1, double k1, double b2, double k2)
+// // {
+// //     if (k1 == k2)
+// //     {
+// //         if (b1 == b2)
+// //         {
+// //             return ("The lines are identical");
 
-        }
+// //         }
 
-        return ("The lines are parallel");
-    }
-    return $"Точка пересечения прямых -> ";
-}
+// //         return ("The lines are parallel");
+// //     }
+// //     return $"Точка пересечения прямых -> ";
+// // }
 
 double EnterNumber(string message)
 
@@ -49,7 +49,7 @@ double b1 = EnterNumber("Введите коэффициент первой пр
 double k1 = EnterNumber("Введите угловой коэффициент первой прямой");
 double b2 = EnterNumber("Введите коэффициент второй прямой");
 double k2 = EnterNumber("Введите угловой коэффициент второй прямой");
-Console.Write(CrossingExist(b1, k1, b2, k2));
+// Console.Write(CrossingExist(b1, k1, b2, k2));
 double[] array = DotCrossing(b1, k1, b2, k2);
 PrintArray(array);
 
